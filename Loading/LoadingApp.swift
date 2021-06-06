041c544a3399e2e48839455a121a14f2db40cc0f
Raphael Cerqueira <raphaelfc@gmail.com>
2021-06-06 15:31:21 -0300

    Initial Commit

---
+++ Loading/LoadingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoadingApp.swift
+//  Loading
+//
+//  Created by Raphael Cerqueira on 06/06/21.
+//
+
+import SwiftUI
+
+@main
+struct LoadingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
