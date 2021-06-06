3aace8c91b58cbd0e36eb8ca5840fe1ec7e2553c
Raphael Cerqueira <raphaelfc@gmail.com>
2021-06-06 17:42:00 -0300

    Primeiro commit

--- Loading/ContentView.swift
+++ Loading/ContentView.swift
@@ -8,9 +8,81 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isLoading = false
+    @State var isAnimating = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        if isLoading {
+            CustomLoadingView()
+                .rotationEffect(.degrees(isAnimating ? 360 : 0))
+                .animation(Animation.linear(duration: 0.75).repeatForever(autoreverses: false))
+                .onAppear {
+                    isAnimating = true
+                }
+                .onDisappear {
+                    isAnimating = false
+                }
+        } else {
+            VStack {
+                Spacer()
+                
+                Button(action: {
+                    self.isLoading.toggle()
+                    
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                        self.isLoading.toggle()
+                    }
+                }, label: {
+                    Text("Start animation")
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 60)
+                        .foregroundColor(.white)
+                        .background(Color.red)
+                        .cornerRadius(8)
+                        .padding()
+                })
+            }
+        }
+    }
+}
+
+struct CustomLoadingView: View {
+    var width: CGFloat = 3
+    var height: CGFloat = 12
+    var spacing: CGFloat = 2
+    
+    var body: some View {
+        ZStack {
+            VStack(spacing: spacing) {
+                Rectangle()
+                    .frame(width: width, height: height)
+                    .foregroundColor(.red)
+                
+                HStack(spacing: spacing) {
+                    Rectangle()
+                        .frame(width: height, height: width)
+                        .foregroundColor(.clear)
+                    
+                    Circle()
+                        .strokeBorder(Color.red, lineWidth: 3)
+                        .frame(width: 16, height: 16)
+                    
+                    Rectangle()
+                        .frame(width: height, height: width)
+                        .foregroundColor(.red)
+                }
+                
+                Rectangle()
+                    .frame(width: width, height: height)
+                    .foregroundColor(.clear)
+            }
+            
+            Rectangle()
+                .frame(width: height, height: width)
+                .foregroundColor(.red)
+                .rotationEffect(.degrees(135))
+                .offset(x: height - 1, y: -height + 1)
+        }
     }
 }
 
---
+++ README.md
@@ -0,0 +1,23 @@
+# Loading do aplicativo da Claro usando SwiftUI
+Implementação da animação de loading do aplicativo da Claro.
+
+## Screenshots
+<p align="center">
+    <img src="https://user-images.githubusercontent.com/16376748/120939375-30500a00-c6ee-11eb-82db-4da17642669e.png" width="400">
+</p>
+
+## Vídeos
+- [Vídeo no Youtube](https://youtu.be/g8QcouifgqI)
+
+## Requisitos
+- iOS 14
+- SwiftUI
+- Xcode 12
+
+## Meta
+- Github: [https://github.com/rphlfc](https://github.com/rphlfc)
+- Instagram: [https://www.instagram.com/rphlfc.dev](https://www.instagram.com/rphlfc.dev)
+- LinkedIn: [http://www.linkedin.com/in/rphlfc](http://www.linkedin.com/in/rphlfc)
+- Twitter: [https://twitter.com/rphlfc](https://twitter.com/rphlfc)
+- Website: [https://rphlfc.github.io](https://rphlfc.github.io)
+- Youtube: [https://www.youtube.com/c/raphaelcerqueira](https://www.youtube.com/c/raphaelcerqueira)
